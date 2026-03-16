07d6b22541001d7da2981de31aa1f7cca9cb7eb2
Afrah <afrahalsnyd@gmail.com>
2026-03-16 13:47:45 +0300

    done

--- pipeline.py
+++ pipeline.py
@@ -10,7 +10,8 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # ─── Configuration ────────────────────────────────────────────────────────────
 
